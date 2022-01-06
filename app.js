123f8096ae70297a18013d51029a988af2850769
Woturios <wtsroboczy@gmail.com>
2022-01-06 22:15:25 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+function scrollToTop() {
+  window.scrollTo(0, 0);
+}
